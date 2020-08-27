a37cd8d3c5de0503335099fcfdc4f52bab0b6d7d
Udar98 <raducristian.vasile@yahoo.com>
2020-08-27 17:41:27 +0300

    Djangogirls page 98 save

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ uni django cv
 test commit/ dev
 
 AFTER I RUN django-admin.exe startproject mysite .
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Chisinau"
+TIME_ZONE = "Europe/Bucharest"
 
 USE_I18N = True
 
